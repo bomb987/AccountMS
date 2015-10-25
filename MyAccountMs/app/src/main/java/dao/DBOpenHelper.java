523a2c83d8efa179274a820addc9f23cad6303bb
guying <18813128083@163.com>
2015-10-25 12:58:18 +0800

    add DBOpenHelper

---
+++ MyAccountMs/app/src/main/java/dao/DBOpenHelper.java
@@ -0,0 +1,29 @@
+package dao;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by sunchenhao on 2015/10/25.
+ */
+public class DBOpenHelper extends SQLiteOpenHelper {
+    private static final int VERSION = 1;
+    private static final String DBNAME = "account.db";
+    public DBOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DBNAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table tb_outaccount(_id integer primary key,money decimal,time varchar(10),"+"type varchar(10),address varchar(100),mark varchar(200))");
+        db.execSQL("create table tb_inaccount(_id integer primary ke,money decimal,time varchar(10),"+"type varchar(10),handler varchar(100),mark varchar(200))");
+        db.execSQL("create table tb_password(varchar(20))");
+        db.execSQL("create table tb_flag(_id integer primary key,flag varchar(200))");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
